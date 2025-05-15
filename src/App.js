c415befdcfacdc6354606cb238f3da9a8d153a97
Shivani Singh <shivanisingh@Shivanis-MacBook-Air.local>
2025-05-15 15:52:47 +0530

    netflix initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { lazy, Suspense, useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+// import "./scss/index.css";
+
+const AppLayout = () => {
+
+    return (<>Hello</>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
